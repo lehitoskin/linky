084f7acbd26bc3461a6331bca2088d8bf9b060b0
Lehi Toskin <lehi@tosk.in>
2016-11-22 20:42:13 -0800

    DList WIP

---
+++ src/linky/DList.cpp
@@ -0,0 +1,24 @@
+// DList.cpp
+
+// add a new element to the end of the list
+void DList::add(int value)
+{
+	if (head == NULL)
+		head = new Node(value);
+	else
+	{
+		// the list is not empty
+		// use node_ptr to traverse the list
+		Node *node_ptr = head;
+
+		while (node_ptr->next != NULL)
+			node_ptr = node_ptr->next;
+
+		// node_ptr->next is NULL so node_ptr points to the last node
+		// create a new node and put it after the last node.
+		node_ptr->next = new Node(value);
+
+		// tell the new node who its prev is
+		node_ptr->next->prev = node_ptr;
+	}
+}
